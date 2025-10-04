95bb8ef9fdb3f325c32884910fd33c3b03c371b5
natanz <halil.cam0020@gmail.com>
2025-10-05 02:52:25 +0300

    getx paketi ile basit bir sayac uygulaması yapıldı

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:karma_projeler/login_panel/login_panel_package.dart';
+import 'package:karma_projeler/getx_tekrar/increment/increment.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(),
-      home: const LoginPanelPackage(),
+      home: const Increment(),
     );
   }
 }
